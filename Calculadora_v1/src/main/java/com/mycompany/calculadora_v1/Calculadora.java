275fa7c6b373f4718d568adabcb4cac5be4b45bb
Your Name <you@example.com>
2021-04-29 19:42:13 -0300

    bien basica

---
+++ Calculadora_v1/src/main/java/com/mycompany/calculadora_v1/Calculadora.java
@@ -0,0 +1,13 @@
+package com.mycompany.calculadora_v1;
+import JFrame.Pantalla_calculadora;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        Pantalla_calculadora pantalla = new Pantalla_calculadora();
+        pantalla.setVisible(true);
+        
+        
+    }
+    
+}
